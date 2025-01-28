5ebc853f1b4b94c73b43e551cb6a4218a3e50cd8
Giorgio Rafael Ataide Guimarães <Jukersrx@gmail.com>
2025-01-28 16:03:32 -0300

    Hora da pratica 4

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = 'Jogo do numero secreto';
-
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha o numero entre 1 e 10';
+let listaDeNumerosSorteados = []
 let numeroSecreto = gerarNumero();
 console.log(numeroSecreto);
 function exibirTexto(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;  
 };
+let numeroLimite = 10
 let tentativas = 0;
 function inicioJogo(){
     exibirTexto('h1', 'Jogo do numero secreto')
@@ -42,8 +39,20 @@ function verificarChute() {
 };
 
 function gerarNumero(){
-    return parseInt(Math.random() * 10 +1);
-    
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite +1);
+    let tamanhoLista = listaDeNumerosSorteados.length
+
+    if (tamanhoLista == numeroLimite){
+        listaDeNumerosSorteados = []; //limpa a lista
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumero();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados, numeroEscolhido)
+        return numeroEscolhido;
+    }
 };
 
 function getDataFromHTML() {
